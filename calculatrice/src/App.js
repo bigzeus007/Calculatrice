b291e24603557c3b54414504e6c8254dd44a665f
said.T <tarhisaid@gmail.com>
2021-08-10 16:43:31 +0100

    Initiatiation

---
+++ calculatrice/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Nombres from './components/Nombres';
+
+function App() {
+  return (
+    <>
+      <Titre />
+      <Affichage/>
+      <Operations/>
+      <Nombres />
+
+       
+    </>
+  );
+}
+
+export default App;
